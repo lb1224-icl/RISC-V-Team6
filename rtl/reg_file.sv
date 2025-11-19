98d4ffca437ca7e93b7c5d209f80c2b88c80e7ab
Ryota7hos1 <rh1524@ic.ac.uk>
2025-11-19 11:36:54 +0000

    add reg_file module of ALU with parameters and input/output logic

---
+++ rtl/reg_file.sv
@@ -0,0 +1,16 @@
+module reg_file #(
+    parameter
+    D_WIDTH = 8,
+    A_WIDTH = 8
+) (
+    input logic ad1,
+    input logic ad2,
+    input logic ad3,
+    input logic we3,
+    input logic wd3,
+    input logic clk,
+    output logic rd1,
+    output logic rd2,
+    output logic a0
+);
+endmodule
