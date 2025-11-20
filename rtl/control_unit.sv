af3e57b7b470a7f21f5f2596ae11759229ce9ff5
EthanCann <ethan.cann44@gmail.com>
2025-11-20 13:40:24 +0000

    finished sign_extend

---
+++ rtl/control_unit.sv
@@ -0,0 +1,9 @@
+module control_unit #(
+    
+) (
+    input logic                    imm_src,
+    input logic  [11:0]            imm,
+    output logic [WIDTH-1:0]  imm_op
+);
+
+endmodule 
--- rtl/sign_extend.sv
+++ rtl/sign_extend.sv
@@ -1,16 +1,30 @@
 module sign_extend #(
     WIDTH = 32
 ) (
-    input logic                    imm_src,
-    input logic  [11:0]            imm,
-    output logic [WIDTH-1:0]  imm_op
+    input logic  [1:0]             imm_src,  // type of instruction
+    input logic  [WIDTH-1:0]       ins,      // entire instruction word
+    output logic [WIDTH-1:0]       imm_op
 );
 
 always_comb
-if (imm_src)
-    if (imm[11])
-        assign imm_op <= {{WIDTH-12{1'b1}}, imm}
-    else 
-        assign imm_op <= {{WIDTH-12{1'b0}}, imm}
+case (imm_src)   // 2: R type -> do no care as this instruction doesn't use immediates
+    0:  if (ins[31])    // I type
+            imm_op <= {{WIDTH-12{1'b1}}, ins[31:20]}
+        else 
+            imm_op <= {{WIDTH-12{1'b0}}, ins[31:20]}
+    1:  if (ins[31])    // S type
+            imm_op <= {{WIDTH-12{1'b1}}, ins[31:25], ins[11:7]}
+        else 
+            imm_op <= {{WIDTH-12{1'b0}}, ins[31:20]}
+    3:  if (ins[31])    // B type
+            imm_op <= {{WIDTH-13{1'b1}}, ins[31], ins[7], ins[30:25], ins[11:8], 0}
+        else 
+            imm_op <= {{WIDTH-13{0'b1}}, ins[31], ins[7], ins[30:25], ins[11:8], 0}
+
+    default: if (ins[31])   
+                imm_op <= {{WIDTH-12{1'b1}}, ins[31:20]}
+             else 
+                imm_op <= {{WIDTH-12{1'b0}}, ins[31:20]}              
+endcase
 
 endmodule
