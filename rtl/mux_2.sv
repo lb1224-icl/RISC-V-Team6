2eff92a8393e050239645d0c51c9a92ec0d87e3b
lb1224-icl <lb1224@ic.ac.uk>
2025-11-21 00:18:58 +0000

    Changed formatting

--- rtl/mux_2.sv
+++ rtl/mux_2.sv
@@ -1,4 +1,4 @@
-module mux #(
+module mux_2 #(
     DATA_WIDTH = 32
 )(
     input   logic [DATA_WIDTH-1:0]  in0,
--- rtl/pc_unit.sv
+++ rtl/pc_unit.sv
@@ -1,23 +1,27 @@
 module pc_unit #(
-    parameter  WIDTH    = 32
+    parameter WIDTH = 32
 ) (
-    input logic   rst,
-    input logic   clk,
-    input logic   pc_src,
-    input logic   [WIDTH-1:0] pc_prev,
-    input logic   [WIDTH-1:0] immop,
-    output logic  [WIDTH-1:0] PC,
-
+    input  logic                 clk,
+    input  logic                 rst,
+    input  logic                 pc_src,
+    input  logic [WIDTH-1:0]     pc_in,
+    input  logic [WIDTH-1:0]     immop,
+    output logic [WIDTH-1:0]     PC
 );
 
+localparam int INCREMENT = 4;
+
+logic [WIDTH-1:0] next_PC;
+
+assign next_PC = pc_src ? (pc_in + immop) : (pc_in + INCREMENT);
+
 always_ff @(posedge clk, posedge rst) begin
-    if(rst) begin 
-        PC <= WIDTH'b0;
+    if (rst) begin
+        PC <= '0;
     end
     else begin
-        PC <= pc_src ? pc_prev + immop : pc_prev + 3'b100;
+        PC <= next_PC;
     end
 end
-
 
 endmodule
